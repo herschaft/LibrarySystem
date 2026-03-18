ae16e93d13395115ebc55e0699c6e71b5f49cd8a
herschaft <isnotwasd@gmail.com>
2026-03-18 11:38:01 -0300

    Second Checkpoint. Previous base was very bad made, so this should be the corrected version with the empty but inicial base for member-service

---
+++ member-service/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+    id("java")
+}
+
+group = "com.herschaft.librarysystem"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.boot:spring-boot-dependencies:4.0.3")
+    }
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    runtimeOnly("org.postgresql:postgresql")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
